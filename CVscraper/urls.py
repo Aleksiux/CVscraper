b98b13aaef5024348b3636fa6e6af02b8bd115a0
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-06-04 22:11:28 +0300

    added main page

--- CVscraper/urls.py
+++ CVscraper/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path('index', views.index, name='index'),
+    path('cv', views.all_cvs, name='cv'),
+    path('cv/scrape_data', views.scrape_data, name='scrape_data')
 ]
--- CVscraper/views.py
+++ CVscraper/views.py
@@ -1,9 +1,28 @@
+from .utils import cvbankas_lt
+from django.contrib.admin.views.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
-# Create your views here.
 
 
 def index(request):
     context = {
     }
     return render(request, "index.html", context)
+
+
+def all_cvs(request):
+    context = {
+        'cvbankas': cvbankas_lt('kaunas', 'python'),
+        'count': len(cvbankas_lt('kaunas', 'python'))
+    }
+    return render(request, "all_cvs.html", context=context)
+
+
+def is_admin(user):
+    return user.is_superuser
+
+
+@login_required  # Ensures that the user is logged in
+@user_passes_test(is_admin)  # Restricts access to admin users
+def scrape_data(request):
+    return render(request, 'all_cvs.html')
